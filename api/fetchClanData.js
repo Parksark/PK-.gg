9b5d0d80d6bb360f96244215be7c0a678e6b6b2f
Parksark <110898028+Parksark@users.noreply.github.com>
2025-04-28 19:07:29 +0900

    Update fetchClanData.js

--- api/fetchClanData.js
+++ api/fetchClanData.js
@@ -1,10 +1,1 @@
-export async function fetchClanData(clanName) {
-  
-  console.log(`클랜 데이터 가져오기: ${clanName}`);
-  return {
-    name: clanName,
-    members: [],
-    avgDamage: 0,
-    playStyle: 'Unknown'
-  };
-}
+
