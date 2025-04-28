feca9691ff594dda4dd7e32a556716f97001a512
Parksark <110898028+Parksark@users.noreply.github.com>
2025-04-28 15:51:20 +0900

    Create Footer.jsx

---
+++ components/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center p-4 border-t mt-8 text-sm text-gray-500">
+      © 2025 PK.GG - All Rights Reserved
+    </footer>
+  );
+};
+
+export default Footer;
