f60af3c712837557efa6b8d633de74f800a75884
Parksark <110898028+Parksark@users.noreply.github.com>
2025-04-28 18:41:14 +0900

    Update fetchClanData.js

--- api/fetchClanData.js
+++ api/fetchClanData.js
@@ -1,5 +1,5 @@
 export async function fetchClanData(clanName) {
-  Authorization: `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI3MDNhNDhhMC0wMjI1LTAxM2UtMzAwYi0wNjFhOWQ1YjYxYWYiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNzQ1MzgwODM3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InViZCJ9.hs5WCvTM6d0W_y0lsYzpbkREq61PD1p7vbibOGTFK3o`,
+  
   console.log(`클랜 데이터 가져오기: ${clanName}`);
   return {
     name: clanName,
