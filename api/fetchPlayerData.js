947c8606ab7df7de5dc1ccd8ae6d21fa2c253a4b
Parksark <110898028+Parksark@users.noreply.github.com>
2025-04-28 19:07:21 +0900

    Update fetchPlayerData.js

--- api/fetchPlayerData.js
+++ api/fetchPlayerData.js
@@ -1,9 +1,1 @@
-export async function fetchPlayerData(playerName) {
 
- console.log(`플레이어 데이터 가져오기: ${playerName}`);
-  return {
-    name: playerName,
-    avgDamage: 0,
-    recentMatches: []
-  };
-}
