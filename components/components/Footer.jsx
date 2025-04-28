f4b41e91dabe662886516e19debd539fa545d8fa
Parksark <110898028+Parksark@users.noreply.github.com>
2025-04-28 15:55:13 +0900

    Delete components/components directory

--- components/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="flex justify-center items-center p-4 border-t mt-8 text-sm text-gray-500">
-      © 2025 PK.GG - All Rights Reserved
-    </footer>
-  );
-};
-
-export default Footer;
